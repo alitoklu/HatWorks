222f1dd73133c35eea4498c1e8a4009e84402f8d
hat231020 <hat231020@hotmail.com>
2021-04-03 00:08:01 +0300

    generalk update

---
+++ HatWorks/Models/helperServices.cs
@@ -0,0 +1,109 @@
+﻿using HatWorks.Tools;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HatWorks.Models
+{
+    public class helperServices
+    {
+        public static dynamic GetById(Entities.AjaxRequest requestobj)
+        {
+            if (new Helper().IsAuthenticate(0, (Helper.UserOperations)Enum.Parse(typeof(Helper.UserOperations), "goruntuleme")))
+            {
+                try
+                {
+                    return string.Format("SELECT * FROM {0} WHERE idd={1}", requestobj.tablename, requestobj.idd).GetDynamicQuery();
+
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+            else
+                return false;
+        }
+
+
+        public static Result DeleteByIdandTableName(Entities.AjaxRequestObject requestobj)
+        {
+
+            Result result = new Result();
+            if (new Helper().IsAuthenticate(0, (Helper.UserOperations)Enum.Parse(typeof(Helper.UserOperations), "goruntuleme")))
+            {
+                requestobj.tablename = requestobj.tablename.Replace("[", "").Replace("]", "");
+                try
+                {
+                    dynamic model = string.Format("SELECT * FROM {0} WHERE idd={1}", requestobj.tablename, requestobj.idd).GetDynamicQuery();
+                    bool hasassings = false;
+
+
+                    if (!hasassings)
+                    {
+                        int logId = new Helper().AddLog(requestobj.idd, new Helper().currentmember.idd, requestobj.tablename, "Delete", JsonConvert.SerializeObject(model));
+                        MySqlCommand cmd = GenericDataAccess.CreateCommand();
+                        cmd.CommandText = string.Format("DELETE FROM {0} WHERE idd ={1}", requestobj.tablename, requestobj.idd);
+                        int recid = new GenericDataAccess().ExecuteNonQuery(cmd);
+                        result.status = true;
+                    }
+                    else
+                    {
+                        result.message = "İlişkili kayıtlar olduğundan silme işlemi gerçekleştirilemedi.";
+                    }
+
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    result.status = false;
+                    result.message = ex.Message;
+                    return result;
+                }
+            }
+            else
+            {
+                result.status = false;
+                result.message = "İzniniz yok";
+                return result;
+            }
+
+
+        }
+
+        public static Result selectoptionvalues(Entities.SelectOptionObject request)
+        {
+            System.Text.StringBuilder result = new System.Text.StringBuilder();
+
+            Result resultObj = new Result();
+            try
+            {
+                MySqlCommand cmd = GenericDataAccess.CreateCommand();
+                string query = "";
+                switch (request.tablename)
+                {
+                    default:
+                        query = "SELECT " + string.Join(",", request.columns) + " FROM " + request.tablename + " WHERE " + (string.IsNullOrEmpty(request.kosul) ? "1=1" : request.kosul) + (string.IsNullOrEmpty(request.orderby) ? "" : " ORDER BY " + request.orderby);
+                        query = new Helper().CleanTexts(query);
+                        break;
+                }
+                System.Data.DataTable table = new System.Data.DataTable();
+                cmd.CommandText = query;
+                table = new GenericDataAccess().ExecuteSelectCommand(cmd);
+
+                for (int i = 0; i < table.Rows.Count; i++)
+                {
+                    result.AppendFormat("<option value='{0}'>{1}</option>", table.Rows[i][request.columns[0]], table.Rows[i][request.columns[1]]);
+                }
+
+                resultObj.status = true;
+                resultObj.message = result.ToString();
+            }
+            catch (Exception) { }
+            return resultObj;
+        }
+    }
+}
---
+++ HatWorks/Tools/Result.cs
@@ -0,0 +1,77 @@
+﻿using HatWorks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HatWorks.Tools
+{
+    public class Result
+    {
+        public bool status { get; set; }
+        public ErrorType errortype { get; set; }
+        public string title { get; set; }
+        public string message { get; set; }
+
+        public Result()
+        {
+            errortype = ErrorType.Success;
+            title = "Dikkat!";
+            message = "";
+        }
+
+        public enum ErrorType
+        {
+            Success = 0,
+            Information = 1,
+            UserError = 2,
+            SystemError = 3
+        }
+    }
+    public class Results
+    {
+        public class Info
+        {
+            public Result result { get; set; }
+            public Info()
+            {
+                result = new Result();
+            }
+        }
+
+        public class DataResult : Result
+        {
+            public object data { get; set; }
+        }
+
+        public class StaticDatas
+        {
+            public List<DataItems> DataList { get; set; }
+            public object Data { get; set; }
+            public Result result { get; set; }
+            public class DataItems
+            {
+                public string id { get; set; }
+                public string text { get; set; }
+                public string custom1 { get; set; }
+                public string custom2 { get; set; }
+            }
+            public StaticDatas()
+            {
+                result = new Result();
+            }
+        }
+
+        public class Member
+        {
+            public Result result { get; set; }
+            public Entities.Member member { get; set; }
+
+            public Member()
+            {
+                result = new Result();
+            }
+        }
+
+    }
+}
